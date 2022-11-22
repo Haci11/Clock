5d8b8163ae84fd6b9b412f48a27f1bd169253794
Haci11 <46707887+Haci11@users.noreply.github.com>
2022-11-22 13:47:03 +0100

    Added clock api and env for the api key

--- src/components/Clock/clock.jsx
+++ src/components/Clock/clock.jsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sun from "../../assets/desktop/icon-sun.svg";
 import Moon from "../../assets/desktop/icon-moon.svg";
 import Arrow from "../../assets/desktop/icon-arrow-down.svg";
+import axios from "axios";
 
-function Clock() {
-  return <div>Clock</div>;
+function Clock({ date, greeting }) {
+  const [locations, setLocations] = useState({});
+
+  const getLocation = async () => {
+    const response = await axios.get(
+      "https://api.ipbase.com/v2/info?apikey=" + process.env.REACT_APP_API_KEY,
+      { method: "GET", headers: { "Content-Type": "application/json" } }
+    );
+
+    console.log(response.data.data.location);
+    const data = response.data.data.location;
+
+    let locationObj = {
+      city: data.city.name,
+      country_code: data.country.alpha2,
+    };
+    setLocations(locationObj);
+    console.log(locationObj);
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  return (
+    <div>
+      <div>{greeting}</div>
+      <p>{date.hours}:</p>
+      <p>{date.minutes}</p>
+      <span>{date.abbreviation}</span>
+      <div>
+        In {locations.city}, {locations.country_code}{" "}
+      </div>
+      <button>
+        <span>more</span>
+        <img src={Arrow} />
+      </button>
+    </div>
+  );
 }
 
 export default Clock;
